053c7034e183c4132dbf1ccf23241e18d63641ca
Lucas Vinícius <lucas@jlvlg.com>
2022-09-12 16:39:50 -0300

    Changes to log output filename

--- workload/run.sh
+++ workload/run.sh
@@ -33,7 +33,7 @@ done
 shift "$((OPTIND - 1))"
 
 function get_date_time() {
-    date_time=($(date +"%F %T"))
+    date_time=($(date +"%F %H-%M-%S"))
     current_date=${date_time[0]}
     current_time=${date_time[1]}
 }
@@ -54,8 +54,8 @@ for script in "$@"; do
     mkdir -p "logs/$script"
     get_date_time
 
-    log_erro="logs/$script/log-erro-$script-$imagem-$current_date-$current_time.csv"
-    log_arquivo="logs/$script/log-$script-$imagem-$current_date-$current_time.csv"
+    log_erro="logs/$script/log-erro-$script-$imagem-${current_date}_$current_time.csv"
+    log_arquivo="logs/$script/log-$script-$imagem-${current_date}_$current_time.csv"
 
     if [ $rmi -eq 0 ]; then
         echo "count,pull_time,instantiate_time,stop_time,container_removal_time,image_removal_time,date,time" >$log_arquivo

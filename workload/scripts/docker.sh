6e3a367213d0729dfeba37779e6078f06b954f8e
PedroViniciusMelo <pedro.vinicius.melo.silva@gmail.com>
2022-09-19 15:25:31 -0300

    Removed unnecessary image use from podman and docker script

--- workload/scripts/docker.sh
+++ workload/scripts/docker.sh
@@ -3,20 +3,20 @@
 function add_container() {
     instantiate_time=0
 
-    if ! docker images | grep -q $imagemsrc$imagem; then
-        if ! docker pull $imagemsrc$imagem >/dev/null 2>/tmp/ERROR; then
+    if ! docker images | grep -q $imagemsrc; then
+        if ! docker pull $imagemsrc >/dev/null 2>/tmp/ERROR; then
             errcount=$((errcount + 1))
             get_date_time
-            echo "Falha ao fazer o download da imagem $imagemsrc$imagem,$(</tmp/ERROR),$current_date,$current_time" >>$log_erro
+            echo "Falha ao fazer o download da imagem $imagemsrc,$(</tmp/ERROR),$current_date,$current_time" >>$log_erro
             return 1
         fi
     fi
 
     start=$(date +%s%N)
-    if ! container=$(docker run -d $imagemsrc$imagem 2>/tmp/ERROR); then
+    if ! container=$(docker run -d $imagemsrc 2>/tmp/ERROR); then
         errcount=$((errcount + 1))
         get_date_time
-        echo "Falha ao instanciar a imagem $imagemsrc$imagem,$(</tmp/ERROR),$current_date,$current_time" >>$log_erro
+        echo "Falha ao instanciar a imagem $imagemsrc,$(</tmp/ERROR),$current_date,$current_time" >>$log_erro
         return 1
     fi
     instantiate_time=$(($(date +%s%N) - start))
